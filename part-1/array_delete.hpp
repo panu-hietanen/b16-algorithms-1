8280c0f5d4bf9abfd737a297493f0f8363dff7e0
panu-hietanen <78078779+panu-hietanen@users.noreply.github.com>
2024-02-03 16:11:21 +0000

    Completed q2

--- part-1/array_delete.hpp
+++ part-1/array_delete.hpp
@@ -4,11 +4,20 @@
 #include <cstddef>
 #include <utility>
 #include <vector>
+#include <cassert>
 
 template <typename T>
 void array_delete(std::vector<T> &A, std::size_t index)
 {
     // WRITE YOUR CODE HERE
+
+
+    assert(index <= A.size());
+    for (auto i = index; i < A.size(); ++i)
+    {
+        A[i] = A[i+1];
+    }
+    A.resize(A.size() - 1);
 }
 
 #endif // __array_delete__
